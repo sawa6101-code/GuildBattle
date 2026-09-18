0fb6410d4d78a66227b7939d0e33d5b9caf681c3
sawa <sawai6101@yahoo.co.jp>
2026-09-18 17:06:09 +0900

    Refresh cache for own member registration fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='guildbattle-v11';
+const CACHE='guildbattle-v12';
 const ASSETS=['./','./index.html','./style.css','./app.js','./lazy-load.js','./manifest.json','./character_master_catalog_20260918.js','./help.js','./phase4_2_character_master.js','./enemy_guild_seed_20260918.js','./own_players_seed_20260918.js','./party_seed_kuroronkotarezo_20260918.js','./persistent_recovery_seed.js','./character_match_engine.js','./character_match_audit.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
