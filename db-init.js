8c8f11b87dd88e12346c886973becbb88824c4ef
sawa <sawai6101@yahoo.co.jp>
2026-09-26 16:51:28 +0900

    fix: IndexedDB v5へ更新して画像ストアを確実に作成

--- db-init.js
+++ db-init.js
@@ -1,7 +1,7 @@
 /* GuildBattle IndexedDB schema bootstrap v3 */
 (function(){
 'use strict';
-const DB='paranoise-guildbattle',VERSION=4;
+const DB='paranoise-guildbattle',VERSION=5;
 const defs={guilds:'id',members:'id',parties:'id',partyCharacters:'id',characters:'id',battleMatches:'id',battleResults:'id',fatigueHistory:'id',screenshots:'id',settings:'key',characterImages:'id',characterScreenshots:'id',characterSkillSources:'id'};
 const open=indexedDB.open(DB,VERSION);
 open.onupgradeneeded=()=>{
